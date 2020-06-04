ad717145afb3f78dd3a1dfdc165281da04ad8e6e
Carlos Pallares <blackparker77@gmail.com>
2020-06-03 20:10:28 -0500

    practica git

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log("Hola Mundo");
+console.log("Esto está muy bacano");
+//primer reto make it real
+console.log("Mi primera cadena de texto.");
+console.log("Texto entre comillas dobles");
+console.log('Texto entre comillas simples');
+
